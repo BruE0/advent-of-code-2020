3354b70047e9caebd8068589c4c835be9a2fea63
BruE0 <bruno7497@gmail.com>
2020-12-04 22:10:00 -0300

    New

--- code/day03.py
+++ code/day03.py
@@ -6,3 +6,40 @@
 """
 
 
+with open("day03_input.txt") as f:
+    grid = f.read().splitlines()
+
+height = len(grid)
+width = len(grid[0])
+
+no_trees_R3_D1 = sum(
+    grid[row_idx][(row_idx * 3) % width] == "#" 
+    for row_idx in range(1, height)
+)
+
+no_trees_R1_D1 = sum(
+    grid[row_idx][row_idx % width] == "#" 
+    for row_idx in range(1, height)
+)
+
+no_trees_R5_D1 = sum(
+    grid[row_idx][(row_idx * 5) % width] == "#" 
+    for row_idx in range(1, height)
+)
+
+no_trees_R7_D1 = sum(
+    grid[row_idx][(row_idx * 7) % width] == "#" 
+    for row_idx in range(1, height)
+)
+
+no_trees_R1_D2 = sum(
+    grid[row_idx][(row_idx // 2) % width] == "#" 
+    for row_idx in range(2, height, 2)
+)
+
+print(no_trees_R3_D1) # 282 (Part One)
+
+print(no_trees_R1_D1 * no_trees_R3_D1 *
+      no_trees_R5_D1 * no_trees_R7_D1 *
+      no_trees_R1_D2)
+# 958815792 (Part Two)
---
+++ code/day04.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""
+    day04.py
+    2020
+"""
+
+# TO DO
